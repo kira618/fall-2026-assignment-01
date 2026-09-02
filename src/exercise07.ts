c4f0486c5904e2b33a2df6070a6259d4c33be32f
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:50:15 -0400

    manual commit

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,22 @@
+import * as fs from "fs";
+import * as path from "path";
+export type Gradebook = {
+  [studentName: string]: {
+    [subject: string]: number;
+  };
+};
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
-}
+ const filePath = path.join(process.cwd(), "data", "gradebook.json");
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  const gradebook: Gradebook = JSON.parse(fileContents);
+
+  let sum = 0;
+  let count = 0;
+
+  for (const student in gradebook) {
+    const grades = gradebook[student];
+    if (grades[subject] !== undefined) {
+      sum += grades[subject];
+      count++;
+    }
+  }
