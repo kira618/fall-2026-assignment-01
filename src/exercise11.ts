baa4e15e60c4114d22a8f5637fbe6d796c1b55a0
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:53:16 -0400

    manual commit

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,25 +1,7 @@
-type RemoteUser = {
-  id: number;
-  name: string;
-  email: string;
-};
-export async function fetchUserEmails(): Promise<string[]> {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-
-  const users: RemoteUser[] = [];
-  for (let i = 0; i < data.length; i++) {
-    users.push({
-      id: data[i].id,
-      name: data[i].name,
-      email: data[i].email,
-    });
-  }
-
-  const emails: string[] = [];
-  for (let i = 0; i < users.length; i++) {
-    emails.push(users[i].email);
-  }
-
-  return emails;
+export async function logStatusToFile(
+  filePath: string,
+  statusMessage: string,
+): Promise<void> {
+  const fs = await import('fs/promises');
+  await fs.writeFile(filePath, statusMessage, 'utf8');
 }
