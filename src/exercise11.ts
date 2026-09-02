450f9594ba972193dc616053f01da9080109dfcb
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:51:59 -0400

    manual commit

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,7 @@
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const fs = await import('fs/promises');
+  await fs.writeFile(filePath, statusMessage, 'utf8');
+}
