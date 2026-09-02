bc46a99dc8f156fc451eb5f449e88ddb594dcc7e
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:53:55 -0400

    manual commit

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,22 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+const promises: Promise<PostItem>[] = [];
+
+  for (let i = 0; i < postIds.length; i++) {
+    const id = postIds[i];
+    const promise = fetch("https://jsonplaceholder.typicode.com/posts/" + id).then(
+      function (response) {
+        return response.json() as Promise<PostItem>;
+      },
+    );
+    promises.push(promise);
+  }
+
+  const results = await Promise.all(promises);
+  return results;
 }
