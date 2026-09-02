76cc30c32dc044ee75dc3212cf20db603605bf43
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:48:44 -0400

    manual commit
    Implement DNA transcription logic to convert DNA to RNA.

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,21 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+let result = "";
+
+  for (let i = 0; i < dna.length; i++) {
+    const letter = dna[i];
+
+    if (letter === "A") {
+      result += "U";
+    } else if (letter === "T") {
+      result += "A";
+    } else if (letter === "C") {
+      result += "G";
+    } else if (letter === "G") {
+      result += "C";
+    } else {
+      throw new Error("Invalid DNA character found: " + letter);
+    }
+  }
+
+  return result;
 }
