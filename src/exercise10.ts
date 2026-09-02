8486b8a2652d7072d563d602207e559c9ea3c398
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:51:45 -0400

    manual commit

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,41 @@
-type UserAccount = {};
+type UserAccount = {
+  id: string;
+  email: string;
+  profile: {
+    name: string;
+  };
+  createdAt: Date;
+};
 
 export class UserRegistry {
-  public registerUser(
-    data: Omit<UserAccount, 'id' | 'createdAt'>,
-  ): UserAccount {
-    return {};
+  users: UserAccount[] = [];
+  nextId: number = 1;
+
+  public registerUser(data: Omit<UserAccount, "id" | "createdAt">): UserAccount {
+    const newUser: UserAccount = {
+      id: "user" + this.nextId,
+      createdAt: new Date(),
+      email: data.email,
+      profile: data.profile,
+    };
+    this.users.push(newUser);
+    this.nextId++;
+    return newUser;
   }
 
   public getUserView(
-    id: string,
-  ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+     id: string,
+  ): Readonly<Pick<UserAccount, "id" | "email" | "profile">> | undefined {
+    const found = this.users.find((user) => user.id === id);
+
+    if (!found) {
+      return undefined;
+    }
+
+    return Object.freeze({
+      id: found.id,
+      email: found.email,
+      profile: found.profile,
+    });
   }
 }
