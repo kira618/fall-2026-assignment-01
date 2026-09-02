08f6e7216f553a8ba4718eaec6d63e730b3f1e22
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:52:24 -0400

    manual commit
    Removed logStatusToFile function and added fetchUserEmails function to retrieve user emails from an API.

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,7 +1,25 @@
-export async function logStatusToFile(
-  filePath: string,
-  statusMessage: string,
-): Promise<void> {
-  const fs = await import('fs/promises');
-  await fs.writeFile(filePath, statusMessage, 'utf8');
+type RemoteUser = {
+  id: number;
+  name: string;
+  email: string;
+};
+export async function fetchUserEmails(): Promise<string[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const users: RemoteUser[] = [];
+  for (let i = 0; i < data.length; i++) {
+    users.push({
+      id: data[i].id,
+      name: data[i].name,
+      email: data[i].email,
+    });
+  }
+
+  const emails: string[] = [];
+  for (let i = 0; i < users.length; i++) {
+    emails.push(users[i].email);
+  }
+
+  return emails;
 }
