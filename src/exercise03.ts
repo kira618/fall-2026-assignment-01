e3fa5c503f2b45c38b7010e2a6069d9276fb01ee
Raki Wane <rwane1801@outlook.com>
2026-09-02 18:49:04 -0400

    manual commit
    Calculate total inventory value for items in stock greater than 5.

--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,16 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
+   const inStock = inventory.filter((item) => {
+    const quantity = item[1];
+    return quantity > 5;
+  });
+
+  const total = inStock.reduce((sum, item) => {
+    const quantity = item[1];
+    const price = item[2];
+    return sum + quantity * price;
+  }, 0);
+
+  return total;
 }
